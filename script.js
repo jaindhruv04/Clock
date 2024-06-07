fde376a4936dd98b9c516aac53be96d6015ba731
Dhruv Jain <dhruvjain0412@gmail.com>
2024-06-07 19:03:13 +0530

    Added AM-PM

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
 var buttonColour = document.querySelector(".button");
+var hourMinute = document.querySelector(".hoursMinutes")
+
 buttonColour.style.color = "red";
 
 
 function toggle() {
     if (buttonColour.style.color === "red") {
         buttonColour.style.color = "blue";
+        buttonColour.textContent = "12-Hour"
     }
     else if (buttonColour.style.color === "blue") {
         buttonColour.style.color = "red";
+        buttonColour.textContent = "24-Hour"
+
     }
 }
 document.querySelector(".button").addEventListener("click", toggle)
@@ -35,11 +40,17 @@ function normalTime() {
 
 function displayTime() {
     var { hour, min, sec, monthName, dayName, date } = normalTime();
-    
-    if (buttonColour.style.color === "red" && hour>12) {
+
+    if (buttonColour.style.color === "red" && hour > 12) {
+        document.querySelector(".hello").textContent = "PM";
+        hourMinute.style.fontSize="110%";
         hour = hour - 12;
     }
-        show(hour, min, sec, monthName, dayName, date);
+    else {
+        document.querySelector(".hello").textContent = "AM";
+        hourMinute.style.fontSize="80%";
+    }
+    show(hour, min, sec, monthName, dayName, date);
 }
 
 setInterval(displayTime, 1000);
