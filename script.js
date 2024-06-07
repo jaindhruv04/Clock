5d216588d29edfa1a494e1daf52446c0815f020b
Dhruv Jain <dhruvjain0412@gmail.com>
2024-06-07 16:52:53 +0530

    Added 24-Hour Toggle Button

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
-function showTime() {
+var buttonColour = document.querySelector(".24-Hour");
+buttonColour.style.color = "red";
+
+
+function toggle() {
+    if (buttonColour.style.color === "red") {
+        buttonColour.style.color = "blue";
+    }
+    else if (buttonColour.style.color === "blue") {
+        buttonColour.style.color = "red";
+    }
+}
+document.querySelector(". 24-Hour").addEventListener("click", toggle)
+
+function normalTime() {
     const time = new Date();
     var date = time.getDate();
     var month = time.getMonth();
@@ -6,17 +20,32 @@ function showTime() {
     var hour = time.getHours();
     var min = time.getMinutes();
     var sec = time.getSeconds();
-    
-    var monthNames = ["January", "February", "March", "April", "May", "June", 
-                      "July", "August", "September", "October", "November", "December"];
+
+    var monthNames = ["January", "February", "March", "April", "May", "June",
+        "July", "August", "September", "October", "November", "December"];
     var dayNames = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-    
+
     var monthName = monthNames[month];
     var dayName = dayNames[day];
+
+    return { hour, min, sec, monthName, dayName, date };
+
+
+}
+
+function displayTime() {
+    var { hour, min, sec, monthName, dayName, date } = normalTime();
     
+    if (buttonColour.style.color === "red" && hour>12) {
+        hour = hour - 12;
+    }
+        show(hour, min, sec, monthName, dayName, date);
+}
+
+setInterval(displayTime, 1000);
+
+function show(hour, min, sec, monthName, dayName, date) {
     document.querySelector(".hoursMinutes").innerHTML = hour + ":" + min;
     document.querySelector(".seconds").innerHTML = sec;
     document.querySelector(".dayDate").innerHTML = monthName + ", " + dayName + ", " + date;
 }
-
-setInterval(showTime, 1000);
